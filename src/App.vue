46e91737515942321c4dea8fd98c8d66d2aa83e0
Kaustubh Saxena <saxena.kaustubh03@gmail.com>
2020-07-18 00:31:11 +0530

    Removed Garbage code

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Home />
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 import Home from './components/Home/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld,
     Home
   }
 }
